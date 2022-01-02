37aaf1e5da3ddd5fb427dde5c01825ad8783a09d
edielam <edguard738@gmail.com>
2022-01-02 12:29:50 +0000

    arrays_modified the print output to not use the Arrays method toString

--- dsa2/src/com/dsa/Multidimension.java
+++ dsa2/src/com/dsa/Multidimension.java
@@ -19,6 +19,11 @@ public class Multidimension {
                 num++;
             }
         }
-        System.out.println(Arrays.toString(arr[1]));
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 }
